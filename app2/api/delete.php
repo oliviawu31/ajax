17f4061c47e52935d5618be27a3dbdfaad26879a
oliviawu <oliviawu0301@gmail.com>
2024-12-24 15:39:05 +0800

    補充在後端取得班級代碼回傳

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -3,8 +3,14 @@ include_once "db.php";
 //處理刪除資料的請求
 $id=$_POST['id'];
 
+// 後端取資料的方式
+// $row=$Stu->find($id);
+
 $Stu->del($id);
 //echo $Stu->del($id);
 echo $_POST['classroom'];
+
+// 後端取資料的方式
+// echo $row['classroom'];
 
 ?>
